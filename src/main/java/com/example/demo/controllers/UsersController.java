6f40cb599aaad55ddd3630812d77b0d7650c7370
PEYZ <zhiani2000@gmail.com>
2023-08-14 14:18:26 -0700

    temp

--- src/main/java/com/example/demo/controllers/UsersController.java
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -1,17 +1,14 @@
 package com.example.demo.controllers;
 
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.models.User;
 import com.example.demo.models.UserRepository;
-
-
 
 @Controller
 public class UsersController {
@@ -21,29 +18,48 @@ public class UsersController {
 
     @GetMapping("/users/view")
     public String getAllUsers(Model model){
-        System.out.println("getting users");
         List<User> users = userRepo.findAll();
         model.addAttribute("users", users);
         return "users/showAll";
     }
 
     @GetMapping("/users/view/{uid}")
-    public String getUser(Model model, @PathVariable String uid){
-        
-        System.out.println("GET User " + uid);
-        // call db
-   
-        model.addAttribute("user");
-        return "showUser";
+    public String getUser(Model model, @PathVariable Integer uid
+    ){
+        Optional<User> user = userRepo.findById(uid);
+        user.ifPresent(value -> model.addAttribute("user", value));
+        return "users/showUser";
     }
 
-    // data coming from form would be a PostMapping
-    @GetMapping(value="/users/add")
-    public String AddUsers() {
-        // get name, size, password from form ...
-        User newuser = new User("henry","1234",25);
-        userRepo.save(newuser);
-        return "users/added";
+    @DeleteMapping("/users/delete/{uid}")
+    public String deleteUser(@PathVariable Integer uid
+    ){
+        userRepo.deleteById(uid);
+        return "redirect:/users/view";  // Redirect to the list of users after deleting
     }
-    
+
+    @DeleteMapping("/users/deleteAll")
+    public String deleteAllUsers(){
+        userRepo.deleteAll();
+        return "redirect:/users/view";  // Redirect to the list of users after deleting all
+    }
+
+    @PostMapping("/users/add")
+    public String addUser(@ModelAttribute User user){
+        userRepo.save(user);
+        return "redirect:/users/view";  // Redirect to the list of users after adding
+    }
+
+    @PutMapping("/users/edit/{uid}")
+    public String editUser(@PathVariable Integer uid, @ModelAttribute User user){
+        Optional<User> existingUser = userRepo.findById(uid);
+        if(existingUser.isPresent()){
+            User dbUser = existingUser.get();
+            dbUser.setName(user.getName());
+            dbUser.setPassword(user.getPassword());
+            dbUser.setSize(user.getSize());
+            userRepo.save(dbUser);
+        }
+        return "redirect:/users/view/{uid}";  // Redirect to the edited user's details page
+    }
 }
